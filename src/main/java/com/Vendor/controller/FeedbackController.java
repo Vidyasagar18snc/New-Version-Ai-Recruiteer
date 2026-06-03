8205fbb5118d75b10d214b200dbe04f881fccc15
Vidyasagar18snc <vidya.sagar@hginfotech.io>
2026-06-03 10:49:13 +0530

    Update FeedbackController.java

--- src/main/java/com/Vendor/controller/FeedbackController.java
+++ src/main/java/com/Vendor/controller/FeedbackController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/feedback")
-@CrossOrigin(origins = "http://localhost:4200")
+
 
 @RequiredArgsConstructor
 public class FeedbackController {
